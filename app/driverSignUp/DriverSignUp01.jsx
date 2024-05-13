897a92d9eb348b49dc2ddc65c1b7db1f0e18da59
Wen Li <weli0091@uni.sydn.edu.au>
2024-05-13 20:12:57 +1000

    Driver signUP page 2 done, now need animations

---
+++ app/driverSignUp/DriverSignUp01.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+export default function DriverSignUp01() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl mb-3">Add Your Details</h1>
+      <div className="mb-5">
+        <label
+          htmlFor="username"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Full Name
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Full Name"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="youremail@gmail.com"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-800 dark:text-white">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Phone
+        </label>
+        <input
+          type="number"
+          id="number"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+
+      <div className="flex items-start mb-5">
+        <div className="flex items-center h-5">
+          <input
+            id="remember"
+            type="checkbox"
+            value=""
+            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+          />
+        </div>
+        <label
+          htmlFor="remember"
+          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+          Remember me
+        </label>
+      </div>
+    </div>
+  );
+}
---
+++ app/driverSignUp/DriverSignUp02.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+export default function DriverSignUp02() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl mb-3">Add Car Details</h1>
+      <div className="mb-5">
+        <label
+          htmlFor="car-make"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Make
+        </label>
+        <input
+          type="text"
+          id="car-make"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="BMW"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="model"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Model
+        </label>
+        <input
+          type="text"
+          id="model"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="youremail@gmail.com"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="colour"
+          className="block mb-2 text-sm font-medium text-gray-800 dark:text-white">
+          Colour
+        </label>
+        <input
+          type="text"
+          id="colour"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="White"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Registration Plate
+        </label>
+        <input
+          type="text"
+          id="email"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="ABC123"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          htmlFor="capacity"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Passenger Capacity
+        </label>
+        <input
+          type="number"
+          min="1"
+          max="8"
+          id="capacity"
+          className="bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="ABC123"
+        />
+      </div>
+    </div>
+  );
+}
