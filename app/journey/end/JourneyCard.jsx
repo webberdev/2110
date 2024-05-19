f7736050c28bdf8e4ae05611eec8bebf6580c748
Wen Li <weli0091@uni.sydn.edu.au>
2024-05-19 16:09:53 +1000

    fixed link bug

--- app/journey/end/JourneyCard.jsx
+++ app/journey/end/JourneyCard.jsx
@@ -51,7 +51,7 @@ export default function JourneyCard() {
         </div>
         <div className="flex flex-col gap-3">
           <a
-            href="#"
+            href="/journey/tip"
             className="inline-flex mx-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 ">
             Tip Driver &rarr;
           </a>
